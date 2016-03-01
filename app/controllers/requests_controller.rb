0bb8f39c331a06e675e128cc87cb4e8dd0dc7a74
Ronny Almog <ronny2205@gmail.com>
2016-03-01 21:53:28 +0000

    creating the request show page

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,26 +1,25 @@
 class RequestsController < ApplicationController
 
-  # Get a request to /:trap_id
+  # Receiving a request at /:trap_id
   def new 
-  	@new_request = Request.create(:trap_name => params[:trap_id], :request_info => request.env, 
+  	new_request = Request.create(:trap_name => params[:trap_id], :request_info => request.env, 
   		:remote_ip => request.remote_ip, :method => request.method, :scheme => request.scheme, 
   		:req_params => request.parameters)
   end 	
 
-  # /:trap_id/requests
+  # Presenting all requests related to the trap
+  # at /:trap_id/requests
   def index
   	@requests = Request.where(:trap_name => params[:trap_id]).order('created_at DESC')	    
   end	
 
-  # /:trap_id/requests/:id
+  # Presenting a specific request
+  # at /:trap_id/requests/:id
   def show
-
+     @req = Request.where(:id => params[:id]).first
   end
 
   private
 
-  # def request_params
-  #   params.require(:request).permit(:trap_name, :request_info)
-  # end
   	
 end
