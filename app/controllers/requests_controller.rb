007e05639a5fbfd628f2569860efd3b89a249210
Ronny Almog <ronny2205@gmail.com>
2016-03-01 23:01:52 +0000

    sending a response

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -5,18 +5,29 @@ class RequestsController < ApplicationController
   	new_request = Request.create(:trap_name => params[:trap_id], :request_info => request.env, 
   		:remote_ip => request.remote_ip, :method => request.method, :scheme => request.scheme, 
   		:req_params => request.parameters)
+  	if new_request.save
+      render :nothing => true, :status => 200
+    else
+      render :nothing => true, :status => 400
+    end
   end 	
 
   # Presenting all requests related to the trap
   # at /:trap_id/requests
   def index
   	@requests = Request.where(:trap_name => params[:trap_id]).order('created_at DESC')	    
+    if @requests.blank?
+	   render :text => "404 Not Found", :status => :not_found
+	end
   end	
 
   # Presenting a specific request
   # at /:trap_id/requests/:id
   def show
      @req = Request.where(:id => params[:id]).first
+     if @req.blank?
+	   render :text => "404 Not Found", :status => :not_found
+	 end
   end
 
   private
