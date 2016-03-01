645f0bd3ee35017001a7280a7ccb2dd5e74d3b23
Ronny Almog <ronny2205@gmail.com>
2016-03-01 01:36:03 +0000

    update the controller and view

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,18 +1,26 @@
 class RequestsController < ApplicationController
 
-  # /:trap_id
+  # Get a request to /:trap_id
   def new 
+  	@new_request = Request.create(:trap_name => params[:trap_id], :request_info => request.env)
+  	#new_request = Request.new trap_name: params[:trap_id], request_info: request.env
 
   end 	
 
   # /:trap_id/requests
   def index
-  		
+  	@requests = Request.where(:trap_name => params[:trap_id])	    
   end	
 
   # /:trap_id/requests/:id
   def show
 
   end
+
+  private
+
+  # def request_params
+  #   params.require(:request).permit(:trap_name, :request_info)
+  # end
   	
 end
